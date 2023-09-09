def25ec2244fcba537f53c620f0254d37a9d3cb7
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-09 15:11:42 +0330

    added some scripts to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,20 @@
+// Forcing NextJS To Render This Component As Client Side Component
+'use client';
+
+// Importing Part
+import {useRouter} from "next/navigation";
+
 // Defining Title Of Page
 export const metadata = {title: 'داشبورد کاربر - خانه'}
 
-// Creating And Exporting Home Page As Default
+// Creating A Home Page That Checks If User Is Logged In Then Redirects To Dashboard Page. Otherwise, Redirects To Login Page
 export default function HomePage() {
-    // Returning JSX
-    return (
-        <h1>Home Page</h1>
-    );
+    // Defining Router Hook To Use Later
+    const routerHook = useRouter();
+
+    if (localStorage.getItem('user-logged-in') === 'true') {
+        routerHook.push('/dashboard');
+    } else {
+        routerHook.push('/login');
+    }
 }
