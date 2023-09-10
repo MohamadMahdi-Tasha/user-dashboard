f1b0e1f0f1a41612a13483d282b9063059a05d66
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-10 12:28:48 +0330

    added scripts of signup page

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,15 +1,22 @@
+// Forcing NextJS To Render This Component As Client Side Component
+'use client';
+
 // Importing Part
 import Link from 'next/link';
 import InputWithLabelComponent from "@/chunk/input/inputWithLabelComponent";
 import ImageValidationInputComponent from "@/chunk/input/imageValidationInputComponent";
 import NameAndLogoComponent from "@/component/nameAndLogoComponent";
 import WelcomeMassageComponent from "@/component/welcomeMassageComponent";
+import {useRouter} from "next/navigation";
 
 // Defining Title Of Page
 export const metadata = {title: 'داشبورد کاربر - ثبت نام'}
 
 // Creating And Exporting Sign Up Page
 export default function SignUpPage() {
+    // Defining useRouter Hook To Use Later
+    const routerHook = useRouter();
+
     // Returning JSX
     return (
         <div className={'flex lg:h-[100vh] lg:flex-row gap-[20px] flex-col-reverse bg-darkBlue/20'}>
@@ -17,7 +24,12 @@ export default function SignUpPage() {
             <div className={'overflow-auto lg:w-[50%] w-full'}>
                 <div className={'p-[20px] max-w-[1000px] mx-auto'} >
                     <WelcomeMassageComponent />
-                    <form action="#" className={'bg-white rounded-[20px] p-[20px] mb-[40px]'}>
+                    <form onSubmit={(event) => {
+                        event.preventDefault();
+
+                        localStorage.setItem('user-logged-in', 'true');
+                        routerHook.push('/dashboard');
+                    }} action="#" className={'bg-white rounded-[20px] p-[20px] mb-[40px]'}>
                         <h5 className={'text-center font-bold text-darkBlue text-[20px] mb-[20px]'}>ثبت نام</h5>
                         <InputWithLabelComponent id={'username'} label={'نام کاربری'} type={'text'} required={true} />
                         <InputWithLabelComponent id={'email'} label={'ایمیل'} type={'email'} required={true} />
