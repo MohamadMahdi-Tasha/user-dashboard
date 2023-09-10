7f5bcb92b6c063cb8ab19dd78d5592a09cc49f04
Mahdi Tasha <imwhdiiii@gmail.com>
2023-09-10 13:07:19 +0330

    added styles of last commit

--- component/dashboard/dashboardMainHeaderComponent.jsx
+++ component/dashboard/dashboardMainHeaderComponent.jsx
@@ -5,25 +5,30 @@ import Link from "next/link";
 export default function DashboardMainHeaderComponent() {
     // Returning JSX
     return (
-        <header>
-            <Link href={'/dashboard/money'}>
-                <button tabIndex={-1}>
-                    <span className={'bi bi-plus'} />
-                    <span>{(500000).toLocaleString()}</span>
-                    <span>ریال</span>
+        <header className={'flex items-center justify-between flex-wrap gap-[20px] mb-[30px]'}>
+            <div className={'flex gap-[10px]'}>
+                <button className={'bg-darkBlue/30 rounded-[10px] w-[35px] lg:hidden flex items-center justify-center'}>
+                    <span className={'bi bi-list text-white'} />
                 </button>
-                <div>
-                    <Link href={'/dashboard/support'}><button tabIndex={-1}><span className={'bi bi-headphones'} /></button></Link>
-                    <Link href={'/dashboard/notifications'}><button tabIndex={-1}><span className={'bi bi-bell'} /></button></Link>
-                    <Link href={'/dashboard/profile'}>
-                        <button tabIndex={-1}>
-                            <span className={'bi bi-person'} />
-                            <span>محمد مهدی تاشا</span>
-                            <span className={'bi bi-chevron-down'} />
-                        </button>
-                    </Link>
-                </div>
-            </Link>
+                <Link href={'/dashboard/money'}>
+                    <button tabIndex={-1} className={'px-[10px] py-[5px] bg-darkBlue/30 text-white flex items-center rounded-[10px] gap-[10px] transition-all duration-500'}>
+                        <span className={'bi bi-plus'} />
+                        <span>{(500000).toLocaleString()}</span>
+                        <span>ریال</span>
+                    </button>
+                </Link>
+            </div>
+            <div className={'flex gap-[30px]'}>
+                <Link className={'block'} href={'/dashboard/support'}><button className={'bg-darkBlue/30 h-full rounded-[10px] aspect-square'} tabIndex={-1}><span className={'bi bi-headphones text-white'} /></button></Link>
+                <Link className={'block'} href={'/dashboard/notifications'}><button className={'bg-darkBlue/30 h-full rounded-[10px] aspect-square'} tabIndex={-1}><span className={'bi bi-bell text-white'} /></button></Link>
+                <Link href={'/dashboard/profile'}>
+                    <button className={'px-[10px] py-[5px] bg-darkBlue/30 text-white flex items-center rounded-[10px] gap-[10px] transition-all duration-500'} tabIndex={-1}>
+                        <span className={'bi bi-person'} />
+                        <span>محمد مهدی تاشا</span>
+                        <span className={'bi bi-chevron-down'} />
+                    </button>
+                </Link>
+            </div>
         </header>
     );
 }
